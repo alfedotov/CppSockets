f2c485423c2ebf83ef3cb53eae1c4db5112ed641
nickfeisst <76674720+nickfeisst@users.noreply.github.com>
2022-03-21 14:11:50 +1300

    Add methods to determine socket/connection validity

--- TcpSocket.hpp
+++ TcpSocket.hpp
@@ -66,14 +66,24 @@ class TcpSocket : public Socket {
             return (size_t)send(_conn, (const char *)buf, len, 0) == len;
         }
 
-
         bool receiveData(void *buf, size_t len)
         {
             return (size_t)recv(_conn, (char *)buf, len, 0) == len;
         }
+
         bool isConnected()
         {
             return _connected;
+        }
+
+        bool hasValidSocket()
+        {
+            return _sock != INVALID_SOCKET;
+        }
+
+        bool hasValidConnection()
+        {
+            return _conn != INVALID_SOCKET;
         }
 
 };
